fd1e7e1b6752d37de1c03dd2b9aedaa0d8251a5e
Zied Ben Haj Salah <ziedhajsalah@gmail.com>
2016-12-21 16:30:14 +0100

    use helmet to add security headers

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express')
 const path = require('path')
 // const favicon = require('serve-favicon')
 const logger = require('morgan')
+// help secure Express/Connect apps with various HTTP headers
+const helmet = require('helmet')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
@@ -39,6 +41,7 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+app.use(helmet())
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(logger('dev'))
